b4a5973e123858169d25529a576c7cd2abd4b41b
Anumala Sushrai <sushraireddy514@gmail.com>
2025-05-25 11:51:48 +0530

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Kidney-Disease-Classfication
+## 🩺 Kidney Disease Classification - MLflow & DVC
+
+### 🚀 Setup Instructions (Completed Steps)
+
+```bash
+# 1. Clone the repository
+git clone https://github.com/sushrai1/Kidney-Disease-Classfication.git
+cd Kidney-Disease-Classfication
+
+# 2. Create and activate conda environment
+conda create -n kidney310 python=3.10 -y
+conda activate kidney310
+
+# 3. Install project requirements
+pip install -r requirements.txt
+```
---
+++ template.py
@@ -0,0 +1,41 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO,format='[%(asctime)s]: %(message)s:')
+
+project_name='cnnClassifier'
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html"
+]
+
+for filepath in list_of_files:
+    # Create the directory if it doesn't exist
+    dirpath = os.path.dirname(filepath)
+    if dirpath != "":
+        os.makedirs(dirpath, exist_ok=True)
+
+    # Create file if it doesn't exist or is empty
+    if not os.path.exists(filepath) or os.path.getsize(filepath) == 0:
+        with open(filepath, 'w') as f:
+            pass
+        logging.info(f"Creating empty file: {filepath}")
+    else:
+        logging.info(f"File already exists: {filepath}")
+
+
